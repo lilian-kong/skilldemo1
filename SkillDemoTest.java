8f53b219996065c75bc1007ece967b8f877ff2cf
lilian-kong <llkong@ucsd.edu>
2022-04-30 17:39:39 -0700

    added test file

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(0, SkillDemo.add(1,1));
+    }
+}
